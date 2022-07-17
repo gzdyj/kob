1f2418cd3d6f6a9f33572915c39dfaecd2564361
gzdyj <86736734@qq.com>
2022-07-17 19:16:18 +0800

    实现了导航栏+对战页面的地图和障碍物

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,38 +1,18 @@
 <template>
-  <div id="nav">
-      <div>Bot名称：{{ bot_name }}</div>
-      <div>Bot战力：{{ bot_rating }}</div>
-
-  </div>
+  <NavBar/>
   <router-view/>
 </template>
 
 
 <script>
 
-import $ from 'jquery';
-import { ref } from 'vue';
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 export default {
-  name: "App",
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getbotinfo/",
-      type: "get",
-      success: resp => {
-        bot_name.value = resp.name;
-        bot_rating.value = resp.bot_rating;
-      }
-    });
-
-    return {
-      bot_name,
-      bot_rating,
-    }
-
+  components:{
+    NavBar
   }
 
 }
@@ -42,8 +22,17 @@ export default {
 <style>
 body {
 
-  background-image: url("C:/Users/14700/Desktop/sping/kob/web/src/assets/background.png");
+  background-image: url("./assets/images/background.png");
+  /* background-position: center center;
   background-size: cover;
+  background-repeat: no-repeat;
+  width: 100vw; 
+  height: 100vw; */
+  width: 100vw;
+  height: 100vw;
+
+  background-size: cover;
+
 
 }
 </style>
